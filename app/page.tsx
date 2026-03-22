a127fea7cd3b45d82d42f46bd7df17278be17175
anushasubedi <anushasubedi26@gmail.com>
2026-03-22 22:31:25 +1100

    merge conflict resolve

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { motion } from 'framer-motion'
 import TetoPear from './components/TetoPear'
 import Navigation from './components/Navigation'
 import ParticleSystem from './components/ParticleSystem'
-import './styles.css'
 
 export default function Home() {
   const shapeVariants = {
